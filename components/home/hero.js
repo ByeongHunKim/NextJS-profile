5a6466e5058bef9e8b3db63943506dba71841eaf
ByeongHunKim <rkwhr912@naver.com>
2022-07-21 19:07:54 +0900

    7월 21일 목요일 수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -16,13 +16,19 @@ export default function Hero() {
             </h1>
             <p className="mb-8 leading-relaxed">
               좋아하는 체인은 이더리움,폴리곤 그리고 솔라나 입니다. 블록체인 노드 운영 및 web3 에
-              관심이 있으며, 현재는 solana.py를 사용하여 지갑연동 및 runnerM 프로젝트의 솔라나
-              로직을 담당하고 있습니다.
+              관심이 있으며, 현재는 solana.py를 사용하여 지갑연동 및 프로젝트의 솔라나 로직을
+              담당하고 있습니다.
             </p>
             <div className="flex justify-center">
               <Link href="/projects">
                 <a className="btn-blog">블로그 보러가기</a>
               </Link>
+            </div>
+            <br />
+            <div className="flex justify-center">
+              <a className="btn-blog" href="https://velog.io/@hunsm4n">
+                Velog 보러가기
+              </a>
             </div>
           </div>
         </div>
